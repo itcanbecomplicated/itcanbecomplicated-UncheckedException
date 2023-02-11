9304c5358b7c6cfda2ccd53fb53570a4d86f514a
Nikita <itcanbecomplicatedinfo@gmail.com>
2023-02-11 23:32:45 +0000

    lab1.2

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -14,6 +14,7 @@ public class Lab {
      * event happens, like running out of computer memory.
      */
     public void throwUncheckedException(){
-
+        int[] arr = new int[20];
+        int a = arr[24];
     }
 }
